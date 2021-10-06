bad813eb76fdab6663a24b9e7274847acf5744dc
kzavorotnii <67904809+kzavorotnii@users.noreply.github.com>
2021-10-06 14:09:50 +0300

    Update offline.js

--- script/offline.js
+++ script/offline.js
@@ -1,3 +1,7 @@
-function myFunction() {
+function funWrite() {
     document.getElementById("demo").innerHTML = "Hello World";
+}
+
+function funClear() {
+    document.getElementById("demo").innerHTML = "";
 }
